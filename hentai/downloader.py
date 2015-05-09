7152ec2e1a946f25d7b06244291c1b9c8ce0cb76
ricterz <ricterzheng@gmail.com>
2015-05-09 14:00:03 +0800

    global timeout

--- hentai/downloader.py
+++ hentai/downloader.py
@@ -1,11 +1,17 @@
 #coding: utf-8
 import os
 import sys
+import socket
 import threading
 import Queue
 import requests
 from urlparse import urlparse
 from hentai.logger import logger
+
+
+# global timeout
+timeout = 30
+socket.setdefaulttimeout(timeout)
 
 
 class Downloader(object):
@@ -34,7 +40,7 @@ class Downloader(object):
         filename = filename if filename else os.path.basename(urlparse(url).path)
         try:
             with open(os.path.join(folder, filename), "wb") as f:
-                response = requests.get(url, stream=True)
+                response = requests.get(url, stream=True, timeout=10)
                 length = response.headers.get('content-length')
                 if length is None:
                     f.write(response.content)
