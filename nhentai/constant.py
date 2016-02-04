376cca82a34df1defcd33a0979531532bb90eab1
Ricter Z <ricterzheng@gmail.com>
2016-02-04 10:30:23 +0800

    rerange url

--- nhentai/constant.py
+++ nhentai/constant.py
@@ -1,6 +1,6 @@
 SCHEMA = 'http://'
 URL = '%snhentai.net' % SCHEMA
 DETAIL_URL = '%s/g' % URL
-IMAGE_URL = '%si.nhentai.net/galleries' % SCHEMA
 SEARCH_URL = '%s/search/' % URL
+IMAGE_URL = '%si.nhentai.net/galleries' % SCHEMA
 PROXY = {}
