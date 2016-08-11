441317c28c30bac2f3cc791017435d8e5aab44e3
Ricter Z <ricterzheng@gmail.com>
2016-08-11 21:10:11 +0800

    use nhentai mirror

--- nhentai/constant.py
+++ nhentai/constant.py
@@ -1,6 +1,6 @@
 SCHEMA = 'http://'
-BASE_URL = 'nhentai.net'
-# BASE_URL = 'nhentai.ricterz.me'
+# BASE_URL = 'nhentai.net'
+BASE_URL = 'nhentai.ricterz.me'
 URL = '%s%s' % (SCHEMA, BASE_URL)
 DETAIL_URL = '%s/g' % URL
 SEARCH_URL = '%s/search/' % URL
