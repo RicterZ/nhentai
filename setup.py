7c9693785e5cc21e1b97c7f91edef2cef501df37
Ricter Z <ricterzheng@gmail.com>
2018-03-16 23:39:04 +0800

    fix #14

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 # coding: utf-8
 from __future__ import print_function, unicode_literals
+import sys
 import codecs
 from setuptools import setup, find_packages
 from nhentai import __version__, __author__, __email__
+
 
 with open('requirements.txt') as f:
     requirements = [l for l in f.read().splitlines() if l]
 
 
 def long_description():
-    with codecs.open('README.md', 'r') as f:
-        return str(f.read())
+    with codecs.open('README.md', 'rb') as f:
+        if sys.version_info >= (3, 0, 0):
+            return str(f.read())
 
 setup(
     name='nhentai',
