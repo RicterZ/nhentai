fefdd3858a8b27cd41706cd888e5ae9426e37cac
Ricter Z <ricterzheng@gmail.com>
2023-02-07 19:42:27 +0800

    update test

--- tests/test_login.py
+++ tests/test_login.py
@@ -15,6 +15,7 @@ class TestLogin(unittest.TestCase):
         constant.CONFIG['useragent'] = os.getenv('NHENTAI_UA')
 
     def test_cookie(self):
+        raise Exception(constant.CONFIG['cookie'])
         try:
             check_cookie()
             self.assertTrue(True)
